93169a4ff887be7849777e4e55722f9808445e39
John Roller <jtroller33@gmail.com>
2022-11-02 14:57:50 -0500

    working on function to start quiz

--- assets/script.js
+++ assets/script.js
@@ -60,3 +60,20 @@ const questions = [
         correctAns: "",
     }
 ];
+
+let homepageEl = document.getElementById("homepage");
+let timerEl = document.getElementById("timer");
+let startBtn = document.getElementById("startBtn");
+let questionsEl = document.getElementById("questionsPage");
+let choicesEl = document.getElementById("ansChoices");
+let questionTextEl = document.getElementById("questionText");
+let rightOrWrongEl = document.getElementById("rightOrWrong");
+let highScoresEl = document.getElementById("highScores");
+
+function startQuiz() {
+    let time = 60;
+    let currentQuestionIndex = 0;
+    homepageEl.setAttribute("style", "display: none");
+    highScoresEl.setAttribute("style", "display: none");
+    
+}
