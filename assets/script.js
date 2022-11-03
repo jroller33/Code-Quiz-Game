623d01c524c294b0e3ac35385e67128084fb6a94
John Roller <jtroller33@gmail.com>
2022-11-02 19:47:18 -0500

    most functions in script.js are done

--- assets/script.js
+++ assets/script.js
@@ -135,5 +135,101 @@ function newQuestion() {
 }
 
 function endQuiz() {
+    clearInterval(timerId);
+    questionTextEl.setAttribute("style", "display: none");
+    highScoresEl.setAttribute("style", "display: block");
 
+    addInitials();
 }
+
+function addInitials() {
+    userScore = document.createElement("p");
+    initials = document.createElement("p");
+    userInitials = document.createElement("input");
+    submitBtn = document.createElement("button");
+
+    userScore.textContent = `Your time was ${time}`;
+    initials.textContent = "Your initials: ";
+    userInitials.setAttribute("name", "initials");
+    userInitials.setAttribute("placeholder", "Type here");
+    submitBtn.textContent = "Submit";
+    submitBtn.setAttribute("class", "userChoice");
+    submitBtn.setAttribute("id", "submitBtn");
+
+    highScoresEl.appendChild(userScore);
+    highScoresEl.appendChild(initials);
+    highScoresEl.appendChild(userInitials);
+    highScoresEl.appendChild(submitBtn);
+
+    submitBtn.addEventListener("click", inputInitials);
+}
+
+function inputInitials() {
+    if (userInitials.value.length < 2) {
+        initials.textContent = "Add your initials";
+    } else {
+        saveUserScore();
+    }
+}
+
+function saveUserScore() {
+    let scoreArray = [];
+    let userInfo = {
+        userInitials: userInitials.value,
+        score: time
+    }
+
+    let scoreStorage = localStorage.getItem("userInfo");
+    scoreArray = [JSON.parse(scoreStorage)];
+
+    localStorage.setItem("userInfo", JSON.stringify([userInfo]));
+
+    userScore.setAttribute("style", "display: none");
+    userInitials.setAttribute("style", "display: none");
+    initials.setAttribute("style", "display: none");
+    submitBtn.setAttribute("style", "display: none");
+
+    showAllScores();
+}
+
+function showAllScores() {
+    let scoreParentEl = document.getElementById("highScores");
+    let scoreChildEl = document.getElementsByClassName("score");
+
+    if (scoreChildEl.length > 0) {
+        scoreParentEl.removeChild(scoreChildEl);
+    }
+
+    let userData = localStorage.getItem("userInfo");
+    let userDataParse = JSON.parse(userData);
+    userDataParse.forEach((data, i) => {
+        let user = document.createElement("div");
+        user.setAttribute("class", "score");
+        user.textContent = (`${index + 1}. ${info.playerInit}, ${info.score}`);
+
+        highScoresEl.appendChild(user);
+    });
+
+    replay();
+}
+
+function replay() {
+    replayBtn = document.createElement("button");
+    replayBtn.textContent = "Do you want to play again?";
+    replayBtn.setAttribute("class", "choice");
+    replayBtn.setAttribute("id", "replay");
+    highScoresEl.appendChild(replayBtn);
+    replayBtn.addEventListener("click", function() {
+        replayBtn.setAttribute("style", "display: none");
+        startQuiz();
+    });
+};
+
+timerEl.setAttribute("style", "display: none");
+
+highScoresEl.setAttribute("style", "display: none");
+
+startBtn.addEventListener("click", startQuiz);
+
+
+
